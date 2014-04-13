e9720d297421cbf712548d7712c4cf46ea5798b7
Shuai Zhu <zhus@zszs.ihep.ac.cn>
2014-04-13 21:22:03 +0800

    web.py added

---
+++ Web_Py/code01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import web
+
+# URL 规则
+urls = (
+	'/(.*)', 'hello'
+	)
+# 应用程序
+app = web.application(urls, globals())
+class hello:
+	def GET(self, name):
+		if not name: name = 'world'
+		web.header('Content-Type', 'text/html; charset=UTF-8')
+		return '你好，' + '世界' + '！'
+# 启动
+if __name__ == "__main__":
+	app.run()
